488eaf915551cfa6b8ad70cbfcc95e5048534723
Skund <Skund.kr@gmail.com>
2025-03-25 14:40:22 +0530

    images changed from jpg too JPG

--- lib/constants.ts
+++ lib/constants.ts
@@ -24,64 +24,64 @@ export const products = [
   {
     title: 'Editorially',
     
-    thumbnail: '/p1.jpg',
+    thumbnail: '/p1.JPG',
   },
   {
     title: 'Editrix AI',
     
-    thumbnail: '/p5.jpg',
+    thumbnail: '/p5.JPG',
   },
   {
     title: 'Pixel Perfect',
     
-    thumbnail: '/p3.jpg',
+    thumbnail: '/p3.JPG',
   },
 
   {
     title: 'Algochurn',
     
-    thumbnail: '/p1.jpg',
+    thumbnail: '/p1.JPG',
   },
   {
     title: 'Aceternity UI',
     
-    thumbnail: '/p8.jpg',
+    thumbnail: '/p8.JPG',
   },
   {
     title: 'Tailwind Master Kit',
     
-    thumbnail: '/p7.jpg',
+    thumbnail: '/p7.JPG',
   },
   {
     title: 'SmartBridge',
     
-    thumbnail: '/p1.jpg',
+    thumbnail: '/p1.JPG',
   },
   {
     title: 'Renderwork Studio',
    
-    thumbnail: '/p2.jpg',
+    thumbnail: '/p2.JPG',
   },
 
   {
     title: 'Creme Digital',
    
-    thumbnail: '/p7.jpg',
+    thumbnail: '/p7.JPG',
   },
   {
     title: 'Golden Bells Academy',
   
-    thumbnail: '/p4.jpg',
+    thumbnail: '/p4.JPG',
   },
   {
     title: 'Invoker Labs',
    
-    thumbnail: '/p5.jpg',
+    thumbnail: '/p5.JPG',
   },
   {
     title: 'E Free Invoice',
    
-    thumbnail: '/p6.jpg',
+    thumbnail: '/p6.JPG',
   },
 ]
 
