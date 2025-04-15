826abdb967dcec32a404ab837a30710c3904137f
Skund <Skund.kr@gmail.com>
2025-04-15 21:54:56 +0530

    form working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -208,7 +208,7 @@ px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,
                   className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
                 >
                   <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
-                    Fill The Form .
+                    <a href="https://forms.office.com/r/FprYCMy6kw">Fill The Form</a> .
                   </span>
                 </Button>
 
