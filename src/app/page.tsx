c8ce3265d5c9d7412e92a8a879caa13fc38dd48c
Skund <Skund.kr@gmail.com>
2025-04-21 13:45:27 +0530

    venue added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,
                   className="p-8 mb-8 md:mb-0 text-2xl w-full sm:w-fit border-t-2 rounded-full border-[#4D4D4D] bg-[#1F1F1F] hover:bg-white group transition-all flex items-center justify-center gap-4 hover:shadow-xl hover:shadow-neutral-500 duration-500"
                 >
                   <span className="bg-clip-text text-transparent bg-gradient-to-r from-neutral-500 to-neutral-600  md:text-center font-sans group-hover:bg-gradient-to-r group-hover:from-black goup-hover:to-black">
-                     <a href="https://forms.office.com/r/FprYCMy6kw">Register Today !!</a>
+                     <a href="https://forms.office.com/r/FprYCMy6kw">Join Us At 301 ALH</a>
                   </span>
                 </Button>
                 <h1 className="text-5xl md:text-8xl bg-clip-text text-transparent bg-gradient-to-b from-white to-neutral-600 font-sans font-bold ">
